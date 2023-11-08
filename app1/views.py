baa4f786f156a68d14d18aa52797c81be6f65ebd
arijitswain23 <arijitswain369@gmail.com>
2023-11-08 15:39:08 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def Jampandu(request):
+    return HttpResponse (" Hii Jampandu How are You ")
